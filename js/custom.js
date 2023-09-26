64b3fd03474989e57f762ecd7f5cf04a9dae7f86
woojin <133613646+wo0odychan@users.noreply.github.com>
2023-09-26 16:29:44 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,28 @@ $(function () {
             : $('.Header').removeClass('on');
     });
     $('.menu_bar').on('click', function () {
-        $('.menu_bar').toggleClass('on');
+        $('.menu_bar').addClass('on');
         $('.FullMenu').toggleClass('on');
-    })
+    });
+
+    const TabSlide = new Swiper('.tab_slide', {
+        loop: false,
+        autoplay: false,
+        on: {
+            slideChangeTransitionStart: function () {
+                $('.tab_menu li')
+                    .eq(this.realIndex)
+                    .addClass('on')
+                    .siblings()
+                    .removeClass('on');
+
+            }
+
+        }
+    });
+    $('.tab_menu li').on('click', function () {
+        const idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on')
+        TabSlide.slideTo(idx);
+    });
 })
